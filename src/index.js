1cf2f897ac81e54a5cfd2f5d7918a011b9ad14a6
LuisInfo1802 <luissanchezinfo18@gmail.com>
2023-10-03 14:55:55 -0700

    Add access control

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import app from './app.js'
 
 import {PORT,USER,DATABASE,DBHOST,DBPASSWORD,DBPORT} from './config.js'
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Permitir acceso desde cualquier origen
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
 app.listen(PORT)
 
